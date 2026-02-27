046e79d3b64860eaec117e05158cd5ccf08336f1
Prabhakar <codeprabhakar16@gmail.com>
2026-02-27 18:52:46 +0530

    Initial clean commit

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const app = require('./src/app')
+const connectToDb = require('./src/config/db')
+
+connectToDb()
+
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000")
+})
