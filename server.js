e47b560d395ae98142010bdc9280a29ed8d73271
Prabhakar <codeprabhakar16@gmail.com>
2026-03-08 18:35:07 +0530

    fixed bug

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const connectToDb = require('./src/config/db')
 connectToDb()
 
 
-app.listen(3000, () => {
-  console.log("App is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
+
